22e790d72000b5502338d456bd196925c06340c3
Vaibhavnath Jha <Vaibhavnath Jha>
2020-06-13 16:10:57 +0530

    Removed .png files, instead added animation .GIF

--- Data_generator.py
+++ Data_generator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.datasets import make_blobs
-X, y = make_blobs(n_samples=2000, centers=3, n_features=2, random_state=0, cluster_std=0.9)
+
+X, y = make_blobs(n_samples=500, centers=2, n_features=2, random_state=0, cluster_std=0.1)
 random_data = pd.DataFrame(X)
 random_data.columns = ['X','Y']
